a25556a41e5f72df0a6c2d059e29cd2e7b401439
Nakji <yuitfhk@gmail.com>
2025-01-30 22:01:00 +0900

    first init

---
+++ src/main/java/com/swyp8team2/Swyp8team2Application.java
@@ -0,0 +1,13 @@
+package com.swyp8team2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swyp8team2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swyp8team2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/swyp8team2/Swyp8team2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swyp8team2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swyp8team2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
