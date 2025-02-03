51b21e7e608e30a26b7dae136ae3b73922d789ab
wlsh44 <k87913j@naver.com>
2025-02-03 20:31:54 +0900

    feat: 소셜 계정 entity 추가

---
+++ src/main/java/com/swyp8team2/auth/domain/SocialAccount.java
@@ -0,0 +1,41 @@
+package com.swyp8team2.auth.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+public class SocialAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+//    private String email;
+
+    private String socialId;
+
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+
+    public SocialAccount(Long id, Long userId, String socialId, Provider provider) {
+        this.id = id;
+        this.userId = userId;
+//        this.email = email;
+        this.socialId = socialId;
+        this.provider = provider;
+    }
+
+    public static SocialAccount create(Long userId, String socialId, Provider provider) {
+        return new SocialAccount(null, userId, socialId, provider);
+    }
+}
