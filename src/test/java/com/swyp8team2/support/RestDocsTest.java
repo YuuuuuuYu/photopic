2f657a12f89fa77f04eedf9c0060b9ad3ed9d806
wlsh44 <k87913j@naver.com>
2025-02-14 20:23:38 +0900

    test: 테스트 및 docs 설정 환경 추가

--- src/test/java/com/swyp8team2/support/RestDocsTest.java
+++ src/test/java/com/swyp8team2/support/RestDocsTest.java
@@ -1,19 +1,28 @@
 package com.swyp8team2.support;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.swyp8team2.common.presentation.CustomHeader;
 import com.swyp8team2.support.config.RestDocsConfiguration;
+import com.swyp8team2.support.config.TestSecurityConfig;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.headers.HeaderDescriptor;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.request.ParameterDescriptor;
+import org.springframework.restdocs.snippet.Attributes;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 
 @WebMvcTest
 @AutoConfigureRestDocs
-@Import(RestDocsConfiguration.class)
+@Import({RestDocsConfiguration.class, TestSecurityConfig.class})
 @ExtendWith(RestDocumentationExtension.class)
 public abstract class RestDocsTest {
 
@@ -26,4 +35,26 @@ public abstract class RestDocsTest {
     @Autowired
     protected RestDocumentationResultHandler restDocs;
 
+    protected static Attributes.Attribute constraints(String value) {
+        return new Attributes.Attribute("constraints", value);
+    }
+
+    protected static HeaderDescriptor authorizationHeader() {
+        return headerWithName(HttpHeaders.AUTHORIZATION).description("Bearer token");
+    }
+
+    protected static HeaderDescriptor guestHeader() {
+        return headerWithName(CustomHeader.GUEST_ID).description("게스트 Id");
+    }
+
+    protected static ParameterDescriptor[] cursorQueryParams() {
+        return new ParameterDescriptor[]{
+                parameterWithName("cursor").optional().description("페이지 조회 커서 값"),
+                parameterWithName("size").optional().attributes(defaultValue("10")).description("페이지 크기 (기본 값 10)")
+        };
+    }
+
+    protected static Attributes.Attribute defaultValue(String value) {
+        return new Attributes.Attribute("defaultValue", value);
+    }
 }
