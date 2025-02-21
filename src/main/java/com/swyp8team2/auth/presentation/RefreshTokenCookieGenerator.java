210a6e267d08752b07dd452c98b0e4967f099c31
wlsh44 <k87913j@naver.com>
2025-02-21 17:52:53 +0900

    fix: refreshToken 쿠키 관련 설정 수정

--- src/main/java/com/swyp8team2/auth/presentation/RefreshTokenCookieGenerator.java
+++ src/main/java/com/swyp8team2/auth/presentation/RefreshTokenCookieGenerator.java
@@ -2,17 +2,25 @@ package com.swyp8team2.auth.presentation;
 
 import com.swyp8team2.common.presentation.CustomHeader;
 import jakarta.servlet.http.Cookie;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RefreshTokenCookieGenerator {
 
+    @Value("${spring.profiles.active:default}")
+    private String activeProfile;
+
     public Cookie createCookie(String refreshToken) {
         Cookie cookie = new Cookie(CustomHeader.CustomCookie.REFRESH_TOKEN, refreshToken);
         cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setAttribute("SameSite", "None");
-        cookie.setPath("/auth/reissue");
+        if ("local".equals(activeProfile)) {
+            cookie.setSecure(false);
+        } else {
+            cookie.setSecure(true);
+            cookie.setAttribute("SameSite", "None");
+        }
+        cookie.setPath("/");
         cookie.setMaxAge(60 * 60 * 24 * 14);
         return cookie;
     }
