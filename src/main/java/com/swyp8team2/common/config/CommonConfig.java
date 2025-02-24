6a990372a35b8a793d692358b5f7a54bc1e283cf
wlsh44 <k87913j@naver.com>
2025-02-24 10:33:24 +0900

    fix: jpa auditing 추가

--- src/main/java/com/swyp8team2/common/config/CommonConfig.java
+++ src/main/java/com/swyp8team2/common/config/CommonConfig.java
@@ -3,10 +3,12 @@ package com.swyp8team2.common.config;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.time.Clock;
 
 @Configuration
+@EnableJpaAuditing
 @ConfigurationPropertiesScan
 public class CommonConfig {
 
