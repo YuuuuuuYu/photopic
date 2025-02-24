2b9ee31238fa1c2a33d46df0d93fa73fafe2c0cf
wlsh44 <k87913j@naver.com>
2025-02-24 21:06:26 +0900

    test: 내가 투표한 게시글 조회 테스트 코드 추가

--- src/test/java/com/swyp8team2/post/application/PostServiceTest.java
+++ src/test/java/com/swyp8team2/post/application/PostServiceTest.java
@@ -16,6 +16,8 @@ import com.swyp8team2.post.presentation.dto.VoteResponseDto;
 import com.swyp8team2.support.IntegrationTest;
 import com.swyp8team2.user.domain.User;
 import com.swyp8team2.user.domain.UserRepository;
+import com.swyp8team2.vote.domain.Vote;
+import com.swyp8team2.vote.domain.VoteRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.swyp8team2.support.fixture.FixtureGenerator.createImageFile;
+import static com.swyp8team2.support.fixture.FixtureGenerator.createPost;
+import static com.swyp8team2.support.fixture.FixtureGenerator.createUser;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -40,6 +45,9 @@ class PostServiceTest extends IntegrationTest {
 
     @Autowired
     ImageFileRepository imageFileRepository;
+
+    @Autowired
+    VoteRepository voteRepository;
 
     @Test
     @DisplayName("게시글 작성")
@@ -105,10 +113,10 @@ class PostServiceTest extends IntegrationTest {
     @DisplayName("게시글 조회")
     void findById() throws Exception {
         //given
-        User user = createUser(1);
-        ImageFile imageFile1 = createImageFile(1);
-        ImageFile imageFile2 = createImageFile(2);
-        Post post = createPost(user, imageFile1, imageFile2, 1);
+        User user = userRepository.save(createUser(1));
+        ImageFile imageFile1 = imageFileRepository.save(createImageFile(1));
+        ImageFile imageFile2 = imageFileRepository.save(createImageFile(2));
+        Post post = postRepository.save(createPost(user.getId(), imageFile1, imageFile2, 1));
 
         //when
         PostResponse response = postService.findById(post.getId());
@@ -137,13 +145,8 @@ class PostServiceTest extends IntegrationTest {
     @DisplayName("내가 작성한 게시글 조회 - 커서 null인 경우")
     void findMyPosts() throws Exception {
         //given
-        User user = createUser(1);
-        List<Post> posts = new ArrayList<>();
-        for (int i = 0; i < 30; i += 2) {
-            ImageFile imageFile1 = createImageFile(i);
-            ImageFile imageFile2 = createImageFile(i + 1);
-            posts.add(createPost(user, imageFile1, imageFile2, i));
-        }
+        User user = userRepository.save(createUser(1));
+        List<Post> posts = createPosts(user);
         int size = 10;
 
         //when
@@ -161,13 +164,8 @@ class PostServiceTest extends IntegrationTest {
     @DisplayName("내가 작성한 게시글 조회 - 커서 있는 경우")
     void findMyPosts2() throws Exception {
         //given
-        User user = createUser(1);
-        List<Post> posts = new ArrayList<>();
-        for (int i = 0; i < 30; i += 2) {
-            ImageFile imageFile1 = createImageFile(i);
-            ImageFile imageFile2 = createImageFile(i + 1);
-            posts.add(createPost(user, imageFile1, imageFile2, i));
-        }
+        User user = userRepository.save(createUser(1));
+        List<Post> posts = createPosts(user);
         int size = 10;
 
         //when
@@ -181,30 +179,37 @@ class PostServiceTest extends IntegrationTest {
         );
     }
 
-    private Post createPost(User user, ImageFile imageFile1, ImageFile imageFile2, int index) {
-        Post post = postRepository.save(Post.create(
-                user.getId(),
-                "description" + index,
-                List.of(
-                        PostImage.create("뽀또A", imageFile1.getId()),
-                        PostImage.create("뽀또B", imageFile2.getId())
-                ),
-                "shareUrl" + index
-        ));
-        return post;
+    private List<Post> createPosts(User user) {
+        List<Post> posts = new ArrayList<>();
+        for (int i = 0; i < 30; i += 2) {
+            ImageFile imageFile1 = imageFileRepository.save(createImageFile(i));
+            ImageFile imageFile2 = imageFileRepository.save(createImageFile(i + 1));
+            posts.add(postRepository.save(createPost(user.getId(), imageFile1, imageFile2, i)));
+        }
+        return posts;
     }
 
-    private User createUser(int index) {
-        return userRepository.save(User.create("nickname" + index, "profileUrl" + index));
-    }
+    @Test
+    @DisplayName("내가 투표한 게시글 조회 - 커서 null인 경우")
+    void findVotedPosts() throws Exception {
+        //given
+        User user = userRepository.save(createUser(1));
+        List<Post> posts = createPosts(user);
+        for (int i = 0; i < 15; i++) {
+            Post post = posts.get(i);
+            voteRepository.save(Vote.of(post.getId(), post.getImages().get(0).getId(), user.getSeq()));
+        }
+        int size = 10;
 
-    private ImageFile createImageFile(int index) {
-        return imageFileRepository.save(ImageFile.create(
-                new ImageFileDto(
-                        "originalFileName" + index,
-                        "imageUrl" + index,
-                        "thumbnailUrl" + index
-                )
-        ));
+        //when
+        var response = postService.findVotedPosts(user.getId(), null, size);
+
+        //then
+        int 전체_15개에서_맨_마지막_데이터_인덱스 = posts.size() - size;
+        assertAll(
+                () -> assertThat(response.data()).hasSize(size),
+                () -> assertThat(response.hasNext()).isTrue(),
+                () -> assertThat(response.nextCursor()).isEqualTo(posts.get(전체_15개에서_맨_마지막_데이터_인덱스).getId())
+        );
     }
 }
