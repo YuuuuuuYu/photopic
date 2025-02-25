46bd966623023bbb4d4aac905455795a144287c7
Nakji <yuitfhk@gmail.com>
2025-02-25 19:53:03 +0900

    feat: 개발 데이터 생성 시 랜덤 닉네임 적용

--- src/main/java/com/swyp8team2/common/dev/DataInitializer.java
+++ src/main/java/com/swyp8team2/common/dev/DataInitializer.java
@@ -8,10 +8,11 @@ import com.swyp8team2.image.presentation.dto.ImageFileDto;
 import com.swyp8team2.post.domain.Post;
 import com.swyp8team2.post.domain.PostImage;
 import com.swyp8team2.post.domain.PostRepository;
+import com.swyp8team2.user.domain.NicknameAdjective;
+import com.swyp8team2.user.domain.NicknameAdjectiveRepository;
 import com.swyp8team2.user.domain.User;
 import com.swyp8team2.user.domain.UserRepository;
 import com.swyp8team2.vote.application.VoteService;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -26,6 +27,7 @@ import java.util.Random;
 @RequiredArgsConstructor
 public class DataInitializer {
 
+    private final NicknameAdjectiveRepository nicknameAdjectiveRepository;
     private final UserRepository userRepository;
     private final ImageFileRepository imageFileRepository;
     private final PostRepository postRepository;
@@ -34,13 +36,14 @@ public class DataInitializer {
 
     @Transactional
     public void init() {
+        List<NicknameAdjective> adjectives = nicknameAdjectiveRepository.findAll();
         User testUser = userRepository.save(User.create("nickname", "defailt_profile_image"));
         TokenPair tokenPair = jwtService.createToken(testUser.getId());
         System.out.println("accessToken = " + tokenPair.accessToken());
         System.out.println("refreshToken = " + tokenPair.refreshToken());
         List<User> users = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
-            User user = userRepository.save(User.create("nickname" + i, "defailt_profile_image"));
+            User user = userRepository.save(User.create(adjectives.get(i).getAdjective(), "defailt_profile_image"));
             users.add(user);
             for (int j = 0; j < 30; j += 2) {
                 ImageFile imageFile1 = imageFileRepository.save(ImageFile.create(new ImageFileDto("202502240006030.png", "https://image.photopic.site/images-dev/202502240006030.png", "https://image.photopic.site/images-dev/resized_202502240006030.png")));
