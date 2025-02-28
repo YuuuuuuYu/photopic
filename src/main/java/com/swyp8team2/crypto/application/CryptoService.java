288908e10ace3ec606b86c566b27bb9d82f40e5b
wlsh44 <k87913j@naver.com>
2025-02-28 11:24:40 +0900

    fix: encrypt decrypt 잘 안되는 문제 수정

--- src/main/java/com/swyp8team2/crypto/application/CryptoService.java
+++ src/main/java/com/swyp8team2/crypto/application/CryptoService.java
@@ -3,57 +3,36 @@ package com.swyp8team2.crypto.application;
 import com.swyp8team2.common.exception.BadRequestException;
 import com.swyp8team2.common.exception.ErrorCode;
 import com.swyp8team2.common.exception.InternalServerException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.binary.Base64;
+import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
+@RequiredArgsConstructor
 public class CryptoService {
 
-    private static final String ALGORITHM = "AES";
-    private final SecretKey secretKey;
-
-    public CryptoService() throws Exception {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
-        keyGenerator.init(256);
-        this.secretKey = keyGenerator.generateKey();
-    }
+    private final AesBytesEncryptor encryptor;
 
     public String encrypt(String data) {
         try {
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            byte[] encryptedBytes = cipher.doFinal(data.getBytes());
-            return Base64.encodeBase64URLSafeString(encryptedBytes)
-                    .replace('+', 'A')
-                    .replace('/', 'B');
+            byte[] encrypt = encryptor.encrypt(data.getBytes(StandardCharsets.UTF_8));
+            return Base62.encode(encrypt);
         } catch (Exception e) {
-            log.error("encrypt error {}", e.getMessage());
-            throw new InternalServerException(ErrorCode.INTERNAL_SERVER_ERROR);
+            log.debug("encrypt error {}", e.getMessage());
+            throw new BadRequestException(ErrorCode.INVALID_TOKEN);
         }
     }
 
     public String decrypt(String encryptedData) {
         try {
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            byte[] decoded = Base64.decodeBase64(
-                    encryptedData
-                            .replace('A', '+')
-                            .replace('B', '/')
-            );
-            return new String(cipher.doFinal(decoded));
-        } catch (IllegalBlockSizeException | BadPaddingException e) {
+            byte[] decryptBytes = Base62.decode(encryptedData);
+            byte[] decrypt = encryptor.decrypt(decryptBytes);
+            return new String(decrypt, StandardCharsets.UTF_8);
+        } catch (Exception e) {
             log.debug("decrypt error {}", e.getMessage());
             throw new BadRequestException(ErrorCode.INVALID_TOKEN);
-        } catch (Exception e) {
-            log.error("decrypt error {}", e.getMessage());
-            throw new InternalServerException(ErrorCode.INTERNAL_SERVER_ERROR);
         }
     }
 }
