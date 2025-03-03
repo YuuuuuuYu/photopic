b4af6ff1b602f0ebff5fc2cf36ca0497215a0e45
wlsh44 <k87913j@naver.com>
2025-03-03 16:39:48 +0900

    refactor: jwt claim role 추가

--- src/main/java/com/swyp8team2/auth/application/jwt/JwtClaim.java
+++ src/main/java/com/swyp8team2/auth/application/jwt/JwtClaim.java
@@ -1,17 +1,22 @@
 package com.swyp8team2.auth.application.jwt;
 
+import com.swyp8team2.user.domain.Role;
+
 public class JwtClaim {
 
     public static final String ID = "id";
+    public static final String ROLE = "role";
 
     private final String id;
+    private final Role role;
 
-    public JwtClaim(long id) {
+    public JwtClaim(long id, Role role) {
         this.id = String.valueOf(id);
+        this.role = role;
     }
 
-    public static JwtClaim from(long id) {
-        return new JwtClaim(id);
+    public static JwtClaim from(long id, Role role) {
+        return new JwtClaim(id, role);
     }
 
     public Long idAsLong() {
@@ -20,5 +25,9 @@ public class JwtClaim {
 
     public String id() {
         return id;
+    }
+
+    public Role role() {
+        return role;
     }
 }
